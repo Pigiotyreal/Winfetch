7174f996a938fd5e2e4b8d3b00e02a738e9bb6c1
Pigioty <Pigiotyreal@gmail.com>
2023-05-15 15:43:22 -0400

    DIsplaying text to screen

--- src/gui.cpp
+++ src/gui.cpp
@@ -5,26 +5,56 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("Winfetch", 100, 100, 1080, 720, SDL_WINDOW_SHOWN);
+    if (TTF_Init() != 0) {
+        std::cout << "TTF_Init Error: " << TTF_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Winfetch", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
 
     if (win == nullptr) {
         std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Surface *surface = SDL_GetWindowSurface(win);
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    if (surface == nullptr) {
-        std::cout << "SDL_GetWindowSurface Error: " << SDL_GetError() << std::endl;
+    if (ren == nullptr) {
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_UpdateWindowSurface(win);
+    TTF_Font *font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 24);
+
+    if (font == nullptr) {
+        std::cout << "TTF_OpenFont Error: " << TTF_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Color color = {255, 255, 255, 255};
+    SDL_Surface *surface = TTF_RenderText_Solid(font, "Hello World", color);
+
+    if (surface == nullptr) {
+        std::cout << "TTF_RenderText_Solid Error: " << TTF_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(ren, surface);
+
+    if (texture == nullptr) {
+        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Rect dest = {5, 5, surface->w, surface->h};
+
+    SDL_RenderCopy(ren, texture, NULL, &dest);
+    SDL_RenderPresent(ren);
 
     bool quit = false;
     SDL_Event event;
@@ -37,7 +67,12 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
+    TTF_CloseFont(font);
+    TTF_Quit();
     SDL_Quit();
     return 0;
 }
